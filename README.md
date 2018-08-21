babc85eb1c716740df1ec1bbd55c2494258875b2
Dan Shapiro <dshapiro@express.com>
2018-08-21 15:02:43 -0400

    use sha properly

---
+++ README.md
@@ -0,0 +1,1 @@
+Run `node index.js` to see a comparison of md5 and sha256 hashes
--- howtohash.js
+++ howtohash.js
@@ -1,8 +1,29 @@
-const sha = require('sha.js');
+const shajs = require('sha.js');
 const md5 = require('./node_modules/blueimp-md5/js/md5.js');
 const timeit = require('timeit');
+const sha256 = shajs('sha256');
 
-const iterations = 5000;
+const query = "query ProductQuery($productId: String!){product(id: $productId) {crossRelDetailMessage,crossRelProductURL,fabricCare,gender,      internationalShippingAvailable,      listPrice,      name,      productDescription,      productId,      productImage,      productURL,      promoMessage,      salePrice,      breadCrumbCategory {        categoryName,        h1CategoryName,        links {          rel,          href        }        breadCrumbCategory {          categoryName,          h1CategoryName,          links {            rel,            href          }        },      },      colorSlices {        color,        defaultSlice,        ipColorCode,        swatchURL,        imageMap {          LARGE        },        skus {          displayMSRP,          displayPrice,          skuId,          sizeName,          inventoryMessage,          onlineInventoryCount,          inStoreInventoryCount,          sizeExtension1,          sizeExtension2        }      },      relatedProducts {        listPrice,        name,        productId,        productImage,        productURL,        salePrice      },    }    reviewStats (id: $productId) {      totalReviewCount,      averageOverallRating,    }  }";
+
+const variables = JSON.stringify({
+    'color': undefined,
+    'opName': 'MyQuery',
+    'productId': '9163393',
+    'size': undefined,
+    'splat': ''
+});
+
+const value = query + variables;
+const hash = sha256.update(value, 'utf8').digest('hex');
+console.log({value, hash});
+const hash2 = sha256.update(value, 'utf8').digest('hex');
+console.log({hash2});
+const hash3 = shajs('sha256').update(value, 'utf8').digest('hex');
+console.log({hash3});
+const hash4 = shajs('sha256').update(value, 'utf8').digest('hex');
+console.log({hash4});
+
+/*const iterations = 5000;
 //const value = "aoeuasonetbusanotehaoeisraoceib";
 //const value = {'a': 1, 'b': 3, 'c': 5, 'd': {'e': 5, 'f': [1,2,3,4]}};
 const query = "query ProductQuery($productId: String!){product(id: $productId) {crossRelDetailMessage,crossRelProductURL,fabricCare,gender,      internationalShippingAvailable,      listPrice,      name,      productDescription,      productId,      productImage,      productURL,      promoMessage,      salePrice,      breadCrumbCategory {        categoryName,        h1CategoryName,        links {          rel,          href        }        breadCrumbCategory {          categoryName,          h1CategoryName,          links {            rel,            href          }        },      },      colorSlices {        color,        defaultSlice,        ipColorCode,        swatchURL,        imageMap {          LARGE        },        skus {          displayMSRP,          displayPrice,          skuId,          sizeName,          inventoryMessage,          onlineInventoryCount,          inStoreInventoryCount,          sizeExtension1,          sizeExtension2        }      },      relatedProducts {        listPrice,        name,        productId,        productImage,        productURL,        salePrice      },    }    reviewStats (id: $productId) {      totalReviewCount,      averageOverallRating,    }  }";
@@ -23,7 +44,7 @@ useMD5 = (done) => {
 };
 
 useSHA = (done) => {
-  const hash = sha('sha256').update(value, 'utf8').digest('hex');
+  sha.update(value, 'utf8').digest('hex');
   done();
 };
 
@@ -36,4 +57,4 @@ timeit.howlong(iterations, [useMD5, useSHA, useNothing], (err, results) => {
   console.log('MD5', results[1].average_step_runtime);
   console.log('SHA', results[2].average_step_runtime);
   console.log('Nothing', results[3].average_step_runtime);
-});
+});*/
